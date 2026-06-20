ca75fd151ec001980dade6fa7a32eae2c9358449
adityaRaj982 <khushi2012dalip@gmail.com>
2026-06-20 12:23:45 +0530

    Create 6_class_take_input.c

---
+++ 6_class_take_input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+    //declaring an int variable name num with no number 
+    int num;
+    
+    printf("enter the number: ");
+    //taking int variable without number 
+    scanf("%i", &num);
+    //printing the number we enter 
+    printf("number is: %d\n", num);
+}
